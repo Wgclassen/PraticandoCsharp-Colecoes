6687aaee13688898883b4cb2da2920419f9af573
Willian Classen <Wgclassen@gmail.com>
2025-08-16 12:40:31 -0300

    first commit

---
+++ Somavalorarrays/Somavalorarrays/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] doacoes = { 103.54, 259.72, 82.16, 154.87, 364.45, 14.49 };
+double total = 0;
+
+foreach (var valor in doacoes)
+{
+    total += valor;
+}
+Console.WriteLine($"Total de doações: {total}");
