06cbe6e5eb8ae35a5b58aa4b1b29fea713631ca9
Willian Classen <Wgclassen@gmail.com>
2025-08-17 19:56:49 -0300

    ListOrdering

---
+++ ListOrdering/ListOrdering/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] nomes = { "Fernanda", "Eduardo", "Gustavo", "Carolina", "Alice", "Bruno", "Daniel" };
+Array.Sort(nomes);
+
+Console.WriteLine($"O terceiro nome da lista após ordenação é: {nomes[2]}");
