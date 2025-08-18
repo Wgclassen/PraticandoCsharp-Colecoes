a4ab82139ad83aa4be9fe01a069b0c68fd5eb696
Willian Classen <Wgclassen@gmail.com>
2025-08-18 10:36:04 -0300

    Code Decipher

---
+++ CodeDecipher/CodeDecipher/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] numeros = { 5, 42, 8, 11, 23, 1, 14, 30, 19, 27, 36, 2, 50, 7, 18, 9 };
+int[] combinação = new int[3];
+
+
+combinação[0] = Array.IndexOf(numeros, 19);
+combinação[1] = Array.IndexOf(numeros, 42);
+combinação[2] = Array.IndexOf(numeros, 7);
+
+Console.WriteLine($"Combinação do cadeado: {combinação[0]}-{combinação[1]}-{combinação[2]}");
