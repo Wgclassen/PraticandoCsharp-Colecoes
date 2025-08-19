be5b6208f329efe88d4442d0392b11d499622163
Willian Classen <Wgclassen@gmail.com>
2025-08-19 09:57:07 -0300

    GameScores

---
+++ GameScores/GameScores/Program.cs
@@ -0,0 +1,18 @@
+﻿HashSet<int> setScores = new HashSet<int>
+{
+    150,
+    90,
+    200,
+    120,
+    150,
+    80,
+    180,
+    200
+};
+
+List<int> orderedScores = setScores.OrderBy(score => score).ToList();
+Console.WriteLine("Scores únicos em ordem crescente:");
+foreach (int score in orderedScores)
+{
+    Console.WriteLine(score);
+}
