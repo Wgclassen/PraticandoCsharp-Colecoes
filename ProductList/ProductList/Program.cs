800911579e075afdfe54ef2177536fe4654997e7
Willian Classen <Wgclassen@gmail.com>
2025-08-22 16:30:59 -0300

    ListMerge

---
+++ ProductList/ProductList/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> produtosPrincipal = new List<string> { "Mouse", "Teclado", "Monitor", "Joystick"};
+List<string> produtosSecundaria = new List<string> { "Headset", "Webcam", "Smartphone", "Mousepad" };
+
+
+produtosPrincipal.AddRange(produtosSecundaria);
+
+Console.WriteLine("Lista consolidada:");
+foreach (string produto in produtosPrincipal)
+{
+    Console.WriteLine(produto);
+}
