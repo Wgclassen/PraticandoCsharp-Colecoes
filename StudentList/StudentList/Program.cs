25305b6ecefd74752a2f30dd3556d79f50a94eef
Willian Classen <Wgclassen@gmail.com>
2025-08-23 18:58:44 -0300

    StudentList

---
+++ StudentList/StudentList/Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<string, decimal> notas = new Dictionary<string, decimal>
+{
+    { "Ana", 8.75m },
+    { "Bruno", 6.90m },
+    { "Clara", 9.25m }
+};
+
+foreach (var item in notas)
+{
+    Console.WriteLine($"Aluno: {item.Key} - Nota: {item.Value}");
+}
